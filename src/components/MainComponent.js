909d1e092cff421ddd8d47ee449cc24ffd143345
ApurvGupta001 <apurvg.sre001@gmailcom>
2021-08-10 18:14:18 +0530

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -21,10 +21,6 @@ class Main extends Component {
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
     const HomePage = () => {
       return (
@@ -32,6 +28,20 @@ class Main extends Component {
           dish={this.state.dishes.filter((dish) => dish.featured)[0]}
           promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
           leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+    const DishWithId = ({ match }) => {
+      return (
+        <Dishdetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
         />
       );
     };
@@ -46,6 +56,7 @@ class Main extends Component {
             component={() => <Menu dishes={this.state.dishes} />}
           />
           <Route exact path="/contactus" component={Contact} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
